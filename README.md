0ede405207b0cadd8120ab8be4ee4b80a15d2335
Kimaru thagana <macbuntu@localhost.localdomain>
2021-05-24 12:23:51 +0300

    update run instructions in readme

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Use the docker command below to spin rabbitMq and the management console availab
 ```
 docker run  --hostname my-rabbit -p 15672:15672 -p 5672:5672 --name rabbit-server -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password rabbitmq:3-management
 ```
+
+# Run the components
+To run the producer `python3 producer.py`
+
+To run the consumer `python3 consumer.py`
--- producer.py
+++ producer.py
@@ -1,10 +1,19 @@
-import pika
+import pika, json
+QUEUE_NAME = 'Job_id_queue'
+EXCHANGE_NAME = 'JOB SCHEDULER EXCHANGE'
+sample_data = {"job_id":1,
+               "action":'update',
+               "value":432
+               }
 
+# establish synchronous connection
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('localhost', 5672, '/', 
                               pika.PlainCredentials('user', 'password')))
-channel = connection.channel()
+channel = connection.channel() # create channel on next available number
 print(channel)
-channel.basic_publish(exchange='my_exchange', routing_key='test', body='Test!')
+channel.queue_declare(queue=QUEUE_NAME) # declare which queue youll be using. Will create if needed
+channel.basic_publish(exchange=EXCHANGE_NAME, 
+                      routing_key=QUEUE_NAME, body=json.dumps(sample_data))
 
 connection.close()
